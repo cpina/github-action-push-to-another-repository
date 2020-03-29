30ad5ed618bab9aec9822e71bcf6f9f9a3ea492a
cpina <carles@pina.cat>
2020-03-29 17:30:55 +0200

    Does better job with the directory creation, naming, etc.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,18 +6,23 @@ GITHUB_USERNAME="$2"
 GITHUB_REPO="$3"
 GIT_USER_EMAIL="$4"
 
-CLONE_DIR="clone_repo"
+CLONE_DIR=$(mktemp)
 
 # Setup git
 git config --global user.email "$GIT_USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
 
+ls -la "$CLONE_DIR"
+
 # Copy files into the git and deletes all git
+find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | xargs rm -rf # delete all files (to handle deletions)
+
+ls -la "$CLONE_DIR"
+
+cp -r "$FOLDER"/* "$CLONE_DIR"
+
 cd "$CLONE_DIR"
-# find needs to be in the git repository directory
-find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions)
-cp -r "../$FOLDER"/* .
 
 git add .
 git commit --message "Update from $GITHUB_REPOSITORY"

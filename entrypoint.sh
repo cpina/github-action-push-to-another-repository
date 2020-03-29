8956a2d3f2ca9c85925222370e97a44504bcbf10
cpina <carles@pina.cat>
2020-03-29 17:07:19 +0200

    Updates file adding the code to push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,33 @@
 #!/bin/sh -l
 
 echo "Starts"
-echo "1: $1"
-echo "2: $2"
-echo "3: $3"
-echo "Ends
+FOLDER="$1"
+GITHUB_USERNAME="$2"
+GITHUB_REPO="$3"
+
+git config --global user.email "carles@pina.cat"
+git config --global user.name "$GITHUB_USERNAME"
+
+git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
+
+ls -l
+
+cd "$CLONE_DIR"
+# find needs to be in the git repository directory
+find . | grep -v ".git" | grep -v "^\.*$" | xargs rm -rf # delete all files (to handle deletions)
+
+cp -r "../$FOLDER"/* .
+
+echo "After cd $CLONE_DIR"
+
+ls -la
+
+git add .
+git commit --message "Update from $GITHUB_REPOSITORY"
+git push origin master
+
+cd ..
+echo "Done!"
+
+
+echo "Ends"

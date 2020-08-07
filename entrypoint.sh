f32c463e11d35e2bf56c29401a5a80572be1fccf
Devin Nemec <devin.nemec@enterprisedb.com>
2020-08-07 15:49:54 -0500

    allow separate author and destination repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,18 @@ FOLDER="$1"
 GITHUB_USERNAME="$2"
 GITHUB_REPO="$3"
 USER_EMAIL="$4"
+REPO_USERNAME="$5"
+
+if [ -z "$REPO_USERNAME" ] then
+  REPO_USERNAME= $GITHUB_USERNAME
+fi
 
 CLONE_DIR=$(mktemp -d)
 
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
-git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
+git clone "https://$API_TOKEN_GITHUB@github.com/$REPO_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
 
 ls -la "$CLONE_DIR"
 

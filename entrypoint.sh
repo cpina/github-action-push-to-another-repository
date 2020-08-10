b511d6418a66d9fd4c9d90e44db35744ae8db27a
Devin Nemec <devin.nemec@enterprisedb.com>
2020-08-10 09:02:28 -0500

    adding context and setting upstream

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,22 +14,27 @@ fi
 
 CLONE_DIR=$(mktemp -d)
 
+echo "Cloning destination git repository"
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$REPO_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
-
 ls -la "$CLONE_DIR"
 
+echo "Cleaning destination repository of old files"
 # Copy files into the git and deletes all git
 find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
-
 ls -la "$CLONE_DIR"
 
+echo "Copying contents to to git repo"
 cp -r "$FOLDER"/* "$CLONE_DIR"
-
 cd "$CLONE_DIR"
+ls -la
 
+echo "Adding git commit"
 git add .
+git status
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-git push origin master
+
+echo "Pushing git commit"
+git push -u rigin master

3d4da26ca5de58bea21bce5c3ee055c562915edf
Carles Pina i Estany <carles@pina.cat>
2020-12-02 15:43:05 +0000

    Fixed problem deleting files starting with .git (e.g. .gitattributes)

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ ls -la "$CLONE_DIR"
 
 echo "Cleaning destination repository of old files"
 # Copy files into the git and deletes all git
-find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
+find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git$" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
 ls -la "$CLONE_DIR"
 
 echo "Copying contents to git repo"

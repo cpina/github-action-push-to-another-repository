e7185da60ee20d477f95a0a770fa027056894146
Sachin Chougule <87692101+chougule007@users.noreply.github.com>
2021-10-04 15:45:58 +0530

    Updated the entrypoint
    Added `-f` flag. if the tip of your current branch is behind then it will be helpful

--- entrypoint.sh
+++ entrypoint.sh
@@ -75,4 +75,4 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
 echo "git push origin:"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
-git push "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --set-upstream "$TARGET_BRANCH"
+git push -f "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --set-upstream "$TARGET_BRANCH"

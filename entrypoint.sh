33585ea95a845ddde81c6ef4d61dceff5ee5d0a9
Damien Cornu <damienc.cornu@gmail.com>
2023-04-25 14:31:00 +0200

    Create branch if needed option

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ DESTINATION_REPOSITORY_USERNAME="${8}"
 TARGET_BRANCH="${9}"
 COMMIT_MESSAGE="${10}"
 TARGET_DIRECTORY="${11}"
+CREATE_TARGET_BRANCH_IF_NEEDED="${12}"
 
 if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
 then
@@ -70,6 +71,12 @@ git config --global user.name "$USER_NAME"
 
 {
 	git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
+} || {
+  if [ "$CREATE_TARGET_BRANCH_IF_NEEDED" = 'true' ] ; then
+    git clone --single-branch --depth 1 "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
+  else
+    false
+  fi
 } || {
 	echo "::error::Could not clone the destination repository. Command:"
 	echo "::error::git clone --single-branch --branch $TARGET_BRANCH $GIT_CMD_REPOSITORY $CLONE_DIR"
@@ -136,6 +143,11 @@ echo "[+] Set directory is safe ($CLONE_DIR)"
 # Related to https://github.com/cpina/github-action-push-to-another-repository/issues/64 and https://github.com/cpina/github-action-push-to-another-repository/issues/64
 # TODO: review before releasing it as a version
 git config --global --add safe.directory "$CLONE_DIR"
+
+
+if [ "$CREATE_TARGET_BRANCH_IF_NEEDED" = 'true' ] ; then
+  git checkout -b "$TARGET_BRANCH"
+fi
 
 echo "[+] Adding git commit"
 git add .
